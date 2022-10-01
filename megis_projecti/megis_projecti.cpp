4d5037514b7ea5abf2d7a75fa172db067e54e8a6
SamiKamara <nikkanen.sami@gmail.com>
2022-10-01 13:36:13 +0300

    added importat question

--- megis_projecti/megis_projecti.cpp
+++ megis_projecti/megis_projecti.cpp
@@ -4,5 +4,13 @@ int main()
 {
     std::cout << "Hellou megis!\n";
 	std::cout <<"Megavoima on ylivoimaa!\n";
+	std::cout << "Ylivoima on megavoimaa!\n";
+	std::cout << "\n";
+
+	int x;
+
+	std::cout << "Montako megista ajattelit juoda? "; 
+	std::cin >> x; 
+	std::cout << "Ai, ajattelit siis juoda: " << x; 
 }
 
